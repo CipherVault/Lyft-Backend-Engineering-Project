4de45dc7a8028b3c22ace6a748a4db4a1e82e713
Marc Williams <99524774+NitroZues@users.noreply.github.com>
2023-02-27 11:57:48 -0800

    Add files via upload
    Refactoring is the process of improving the design and quality of existing code without changing its functionality. As code ages, it accumulates issues known as "code smells" that make it harder to maintain and modify. Refactoring can help address these issues and improve the overall quality of the codebase. By cleaning up the code, refactoring can make it more maintainable, extensible, and easier to understand.
    
    To improve the code's maintainability and reduce redundancy, we can extract a parent class to hold the common engine_should_be_serviced method and needs_service method. We can also rename the classes to better reflect their purpose and implement inheritance to avoid duplicate functionality. Finally, we can remove the if-else statements in the needs_service method since they only return True or False and replace them with a single return statement.

---
+++ Refactoring-task.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from abc import ABC, abstractmethod
+
+class Car(ABC):
+    def __init__(self, last_service_date):
+        self.last_service_date = last_service_date
+    
+    @abstractmethod
+    def engine_should_be_serviced(self):
+        pass
+    
+    def needs_service(self, years_threshold=4, mileage_threshold=60000):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + years_threshold)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced(mileage_threshold):
+            return True
+        else:
+            return False
+
+class CapuletEngine(Car):
+    def __init__(self, last_service_date, current_mileage, last_service_mileage):
+        super().__init__(last_service_date)
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
+    
+    def engine_should_be_serviced(self, mileage_threshold=30000):
+        return self.current_mileage - self.last_service_mileage > mileage_threshold
+
+class SternmanEngine(Car):
+    def __init__(self, last_service_date, warning_light_is_on):
+        super().__init__(last_service_date)
+        self.warning_light_is_on = warning_light_is_on
+    
+    def engine_should_be_serviced(self, mileage_threshold=None):
+        return self.warning_light_is_on
+
+class WilloughbyEngine(Car):
+    def __init__(self, last_service_date, current_mileage, last_service_mileage):
+        super().__init__(last_service_date)
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
+    
+    def engine_should_be_serviced(self, mileage_threshold=60000):
+        return self.current_mileage - self.last_service_mileage > mileage_threshold
